117d0fec294af4856c95957f5c1c2055eb211ed8
dengshun1025 <ds1025158146@hotmail.com>
2016-08-31 16:08:16 +0800

    马表界面

---
+++ app/src/main/java/com/example/dengshun/test/Activity/StopWatchActivity.java
@@ -0,0 +1,15 @@
+package com.example.dengshun.test.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.dengshun.test.R;
+
+public class StopWatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_watch);
+    }
+}
